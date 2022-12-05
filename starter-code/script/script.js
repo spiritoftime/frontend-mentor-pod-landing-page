23681203c627949ed3ea4b2b62768494ed5c9a5a
spiritoftime <chooliangyi6@gmail.com>
2022-12-05 16:31:52 +0800

    move index.html out of starter-code folder

--- starter-code/script/script.js
+++ starter-code/script/script.js
@@ -1,6 +1,12 @@
-const inputField = document.querySelector("#user-email");
-const validate = () => {
-  if (!inputField.value.contains("@")) {
-    inputField.appendChild("Please input a valid email.");
+const btn = document.querySelector(".access");
+const input = document.querySelector(".email");
+const description = document.querySelector(".document");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!input.value.includes("@")) {
+    const z = document.createElement("p");
+    z.innerHTML = "Please type a correct email";
+    document.body.appendChild(z);
+    console.log("add");
   }
-};
+});
